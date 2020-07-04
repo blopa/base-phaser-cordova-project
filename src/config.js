000b2aac1c5b131d850d91bdb815504df6882e0b
Pablo Benmaman <montenegro.pablo@outlook.com>
2020-07-04 20:07:25 +0200

    widescreen

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import Phaser from 'phaser';
 export default {
     type: Phaser.AUTO,
     parent: 'content',
-    width: 800,
-    height: 600,
+    width: 768,
+    height: 432,
     scale: {
         mode: Phaser.Scale.HEIGHT_CONTROLS_WIDTH,
         autoCenter: Phaser.Scale.CENTER_BOTH,
